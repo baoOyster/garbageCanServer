b535e98749b4504a2d5c7bac676dbef5853e7fd2
Nghiem Gia Bao <bao15022016@gmail.com>
2023-03-10 21:01:56 +0700

    add username validation to registerController

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,9 +1,12 @@
+const User = require('../db/User');
 const {passwordHash} = require('../utils/passwordValidation');
 
 const register = async (req, res, next) => {
     try {
         const {username, password} = req.body;
         if(!username || !password) new Error('Please given valid username and password');
+        const isUserExist = await User.findOne({username: username});
+        if(isUserExist) new Error('The username has already been used');
         const newUser = new User({
             username: username,
             password: passwordHash(password)
@@ -15,4 +18,4 @@ const register = async (req, res, next) => {
     }
 }
 
-module.exports = {register}
+module.exports = register;
